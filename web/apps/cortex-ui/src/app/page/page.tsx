49527ddb4fd6b51d48e43dfb1fa157460e456312
Merlin Reichwald <merlin.reichwald@gmail.com>
2021-05-23 17:01:37 +0200

    chore: add basic webapp structure

---
+++ web/apps/cortex-ui/src/app/page/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './page.module.css';
+
+/* eslint-disable-next-line */
+export interface PageProps {}
+
+export function Page(props: PageProps) {
+  return (
+    <div>
+      <h1>Welcome to page!</h1>
+    </div>
+  );
+}
+
+export default Page;
